003ab7f4295527a58a3042e4dc172a4cacd31113
jorge <jorge@imatec.cl>
2020-10-06 23:01:30 -0300

    Initial structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const PORT = 3000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname +'/public/index.html');
+});
