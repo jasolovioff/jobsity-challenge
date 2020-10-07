8549cfe8788d43d6587299258c3ba75e75076b6e
jorge <jorge@imatec.cl>
2020-10-06 23:56:46 -0300

    Multi room chatting support added

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const port = 3000;
 
-const PORT = 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-})
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname +'/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/room1', (req, res) => {
-  res.sendFile(__dirname +'/public/room1.html');
+  res.sendFile(__dirname + '/public/room1.html');
 });
 
-const stocks = io.of('stocks');
+app.get('/room2', (req, res) => {
+  res.sendFile(__dirname + '/public/room2.html');
+});
 
-stocks.on('connection', (socket) => {
-  socket.on('join', () => {
-    socket.join();
-    stocks.emit('message', `New user joined room 1!`);
+app.get('/room3', (req, res) => {
+  res.sendFile(__dirname + '/public/room3.html');
+});
+
+// tech namespace
+const tech = io.of('/tech');
+
+tech.on('connection', (socket) => {
+  socket.on('join', (data) => {
+    socket.join(data.room);
+    tech.in(data.room).emit('message', `New user joined ${data.room}!`);
   });
 
-  socket.on('message', (msg) => {
-    console.log(`message: ${msg} `);
-    stocks.emit('message', msg);
+  socket.on('message', (data) => {
+    console.log(`message: ${data.msg} `);
+    tech.in(data.room).emit('message', data.msg);
   });
 
   socket.on('disconnect', () => {
     console.log('user disconected');
 
-    stocks.emit('message', 'user disconnected');
+    tech.emit('message', 'user disconnected');
   });
 });
